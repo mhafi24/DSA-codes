aa9ad2c3418f2922f8dba689d9454f93cd4ab83b
mhafi24 <60808982+mhafi24@users.noreply.github.com>
2020-09-12 23:45:50 +0530

    Add files via upload

---
+++ twosum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i,num in enumerate(nums):
+            if target-num in nums:
+                if nums.index(target-num)!=i:
+                    return[i,nums.index(target-num)]
+                
